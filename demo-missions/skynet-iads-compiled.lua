d1fb27481aba8988a7e653a4648fe5c2c69bc117
Markus Karlsson <mve.karlsson@gmail.com>
2023-04-22 00:15:58 +0200

    compiled

--- demo-missions/skynet-iads-compiled.lua
+++ demo-missions/skynet-iads-compiled.lua
@@ -1,4 +1,4 @@
-env.info("--- SKYNET VERSION: baron-branch-mobile | BUILD TIME: 21.04.2023 2010Z ---")
+env.info("--- SKYNET VERSION: baron-branch-mobile | BUILD TIME: 21.04.2023 2155Z ---")
 do
 --this file contains the required units per sam type
 samTypesDB = {
@@ -3610,12 +3610,13 @@ function SkynetIADSSamSite:getActMobile()
 end
 
 function SkynetIADSSamSite:setActMobile(enable, emissionTimeMax, scootDistanceMin, scootDistanceMax, scootZones)
+	if emissionTimeMax then self.mobilePhaseEmissionTimeMax = emissionTimeMax end
+	if scootDistanceMin then self.mobileScootDistanceMin = scootDistanceMin end
+	if scootDistanceMax then self.mobileScootDistanceMax = scootDistanceMax end
+	self:setMobileScootZones(scootZones)
+
 	if not self.actMobile and enable then
 		self.actMobile = true
-		if emissionTimeMax then self.mobilePhaseEmissionTimeMax = emissionTimeMax end
-		if scootDistanceMin then self.mobileScootDistanceMin = scootDistanceMin end
-		if scootDistanceMax then self.mobileScootDistanceMax = scootDistanceMax end
-		self.mobileScootZones = scootZones
 		self.mobilePhaseEvaluateTaskID = mist.scheduleFunction(SkynetIADSSamSite.evaluateMobilePhase,{self},1, 5)
 	elseif self.actMobile and not enable then
 		--TODO: implement this
